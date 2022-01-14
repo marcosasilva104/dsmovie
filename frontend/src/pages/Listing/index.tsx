1fe64ad62bc9aa4650678665486dc3ceea04be61
Marcos Silva <marcosasilva104@gmail.com>
2022-01-14 17:12:53 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,31 @@ import axios from "axios";
 import MovieCar from "components/MovieCard";
 //import MovieScore from "components/MovieScore";
 //import MovieStars from "components/MovieStars";
+import { MoviePage } from "types/movies";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/request";
 
-// FORMA ERRADA
-axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
-    });
+
 
 function Listing() {
+    
+    const [pageNumber, setPageNumber] = useState(0);
+    
+    useEffect(() => {
+            axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+            console.log(response.data);
+        });
+    }, [])
+    // FORMA ERRADA
+    
+    
     return (
         <>
+            <p>{pageNumber}</p>
 
             <Pagination />
 
