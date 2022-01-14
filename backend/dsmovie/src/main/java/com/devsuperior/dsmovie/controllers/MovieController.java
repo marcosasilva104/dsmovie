8176cb4a727b4cb6775de1597239f441724bd7c2
Marcos Silva <marcosasilva104@gmail.com>
2022-01-13 23:59:20 -0300

    Save score

--- backend/dsmovie/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -1,7 +1,4 @@
 package com.devsuperior.dsmovie.controllers;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
